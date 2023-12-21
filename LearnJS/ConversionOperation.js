6828236a83a4cd8dff480e9e077bcb2e9601fa96
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2023-12-21 06:00:33 +0000

    Conversion Of Data Types

---
+++ LearnJS/ConversionOperation.js
@@ -0,0 +1,39 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+let n="33abc"
+console.log(typeof n)
+let value=Number(n)
+console.log(typeof value)
+console.log(value)  //NaN Not a Number
+
+let na=null
+console.log(typeof na)
+let valuee=Number(na)
+console.log(typeof valuee)
+console.log(valuee) //null value is zero
+
+let naa=undefined
+console.log(typeof naa)
+let valueee=Number(naa)
+console.log(typeof valueee)
+console.log(valueee)  //undefined print the NaN
+
+ //"33"=>33
+ //"33abc"=>NaN
+ //true=>1; false=>0;
+ 
+ let isLoggedIn=""
+
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn) //print false
+
+ //"""=>false;
+ //"bhuppi"=>true
+
+ 
