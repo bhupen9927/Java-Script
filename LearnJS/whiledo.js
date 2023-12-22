9f154cb193b1c746ff4ab220d1698c88e4dccea8
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2023-12-22 13:59:09 +0000

    while and do while loop

---
+++ LearnJS/whiledo.js
@@ -0,0 +1,21 @@
+let i=0
+while(i<=10){
+    console.log(`value of index is ${i}`)
+    i=i+2;
+}
+
+let arr=[1,2,3,4,5]
+let j=0
+while(j<arr.length){
+    console.log(arr[j])
+    j++
+}
+
+//do while loop
+
+let h=1
+do{
+    console.log(`h is ${h}`)
+    h++
+}
+while(h<=10)
