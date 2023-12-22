8dc230262d43ace568e53665fdbf8b7bf3093ab1
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2023-12-22 10:58:55 +0000

    Execution Context

---
+++ LearnJS/executioncontext.js
@@ -0,0 +1,13 @@
+/*  Execution Context
+
+1-> GEC (Global Execution Context)
+2-> FEC (Function Execution context)
+3-> EEC (Eval Execution Context)
+
+
+JS Code executes in two phases
+1->Memory Creation Phase
+2->Execution phase
+
+
+*/
